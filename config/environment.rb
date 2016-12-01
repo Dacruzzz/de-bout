82bc73d1c6fba033c43cc44f0672362ef0d4ed4d
Jelte Kroes <jelte.cruz@gmail.com>
2016-12-01 13:08:49 +0000

    sendgrid fix

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
-
-# Initialize the Rails application.
-Rails.application.initialize!
-
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
