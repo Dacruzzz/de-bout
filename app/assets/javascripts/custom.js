196cf60dd8b08b85c80ce9f663bee7012921a99d
Jelte Kroes <jelte.cruz@gmail.com>
2016-12-07 00:47:37 +0000

    layout changes

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var x = screen.height - 150;
+    $('#sectionhome').css('min-height', x);
+    $('#sectionaboutus').css('min-height', x);
+    $('#sectioncontact').css('min-height', x);
+    $('#sectionprices').css('min-height', x);
+    }    
+)
