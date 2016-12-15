189c1a854a9a45cf2251315041083b3b8baa83f0
Jelte Kroes <jelte.cruz@gmail.com>
2016-12-15 01:34:38 +0000

    added navbar fix, buttonwidth fix, images, container scaling

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -8,19 +8,32 @@ $(document).ready(function(){
     $('#sectionaboutus').css('min-height', x);
     $('#sectioncontact').css('min-height', x);
     $('#sectionprices').css('min-height', x);
-
-    var y = screen.width;
-    var ybutton = y/4.25;
-    var yheader = y/1.25;
     
-    if(y > 700){
-        $('.navbw').css('min-width', ybutton);
-    } else {
-        $('.active > a').css('min-width', y);
-    }
-    
-    $('.img-fluid').css('min-width', yheader);
     $('.img-fluid').css('min-height', 150);
     
+    var y = screen.width;
     $('body').css('max-width', y);
+    
+    var setxybutton = y/4.1;
+    $('.navbw').css('min-width', setxybutton);
+    
+    
+    // NAVBAR TOGGLE (ONCLICK)
+    jQuery('body').bind('click', function(e) {
+        if(jQuery(e.target).closest('.navbar').length == 0) {
+            // click happened outside of .navbar, so hide
+            var opened = jQuery('.navbar-collapse').hasClass('collapse in');
+        if ( opened === true ) {
+            jQuery('.navbar-collapse').collapse('hide');
+            }
+        }
+    });
+
+    // RESIZE BUTTONS ON WINDOW RESIZE
+    $(window).resize(function(){
+        var yx = $(this).width();
+        var yxbutton = yx/4;
+        $('.navbw').css('min-width', yxbutton);
+    });
+    
 });
